22c11315a5309956364eb98b2dcb59f0bb15346f
Sushengru <729119693@qq.com>
2019-10-21 13:36:56 +0800

    数据库连接封装-v1.1 仅保留有用的连接

--- DB_Connect/db_connect.py
+++ DB_Connect/db_connect.py
@@ -6,28 +6,6 @@
 import pymysql
 from sqlalchemy import create_engine
 
-# yifan 数据库配置
-config_local = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'root',
-    'password': '060418ssr',
-    'db': 'yifan',
-    'charset': 'utf8',
-    'cursorclass': pymysql.cursors.DictCursor
-}
-
-# user_tag 数据库配置
-config_local_usertag = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'root',
-    'password': '060418ssr',
-    'db': 'user_tag',
-    'charset': 'utf8',
-    'cursorclass': pymysql.cursors.DictCursor
-}
-
 # BI brand 数据库配置
 config_bi_brand = {
     'host': '192.168.12.182',
@@ -38,7 +16,6 @@ config_bi_brand = {
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
-
 
 config_brand_pymysql = {
     'host': '192.168.15.138',
@@ -51,78 +28,10 @@ config_brand_pymysql = {
 }
 
 
-
-# brand数据库
-# def db_brand():
-#     conn_local = pymysql.connect(**config_bi_brand)
-#     return conn_local
-#
-#
-# def db_brand2():
-#     connstr = 'mysql+pymysql://yifan:yifan@192.168.12.182/brand?charset=utf8mb4'
-#     engine = create_engine(connstr, echo=True)
-#     return engine
-
-
-# yifan数据库
-def db_local_yifan():
-    conn_local = pymysql.connect(**config_local)
-    return conn_local
-
-
-def db_local_yifan2():
-    connstr = 'mysql+pymysql://root:060418ssr@localhost/yifan?charset=utf8'
-    engine = create_engine(connstr, echo=True)
-    return engine
-
-
-# user_tag数据库
-def db_local_usertag():
-    conn_local = pymysql.connect(**config_local_usertag)
-    return conn_local
-
-
 # 服务器数据库
 def db_brand_pymysql():
     conn_local = pymysql.connect(**config_brand_pymysql)
     return conn_local
-
-
-# 自定义数据库链接（导入用）
-def db_conn(schema_name):
-    connstr = 'mysql+pymysql://root:060418ssr@localhost/{}?charset=utf8'.format(schema_name)
-    engine = create_engine(connstr, echo=True)
-    return engine
-
-
-# crm_db 数据库配置
-crm_db = {
-    'host': '192.168.1.236',
-    'port': 3306,
-    'user': 'tang',
-    'password': 'wytec',
-    'db': 'crm_db',
-    'charset': 'utf8',
-    'cursorclass': pymysql.cursors.DictCursor
-}
-
-
-# yifan数据库
-def db_crm():
-    conn_local = pymysql.connect(**crm_db)
-    return conn_local
-
-
-def db_crm2():
-    connstr = 'mysql+pymysql://tang:wytec@192.168.1.236/crm_db?charset=utf8'
-    engine = create_engine(connstr, echo=True)
-    return engine
-
-
-def db_longyi():
-    connstr = 'mysql+pymysql://guest:05160824@192.168.13.95/ddmx?charset=utf8'
-    engine = create_engine(connstr, echo=True)
-    return engine
 
 
 def db_brand():
