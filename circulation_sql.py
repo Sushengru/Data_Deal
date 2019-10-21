00e04f27bbc2ea0850745ee6139cbd91c2da41a8
Sushengru <729119693@qq.com>
2019-10-21 14:43:01 +0800

    封装循环查询-v1.0 添加参数判定是否需要2年内

--- circulation_sql.py
+++ circulation_sql.py
@@ -10,35 +10,65 @@ from dateutil.relativedelta import relativedelta  # 实现日期做差
 
 
 # 按月实现新老客、累计老客的循环查询sql
-def store_sql(start_date, end_date):
+def store_sql(start_date, end_date, if_limit=1):
 
-    # 需要一个参数，将时间减去2年
-    date_diff = start_date - relativedelta(years=2)
+    if if_limit == 1:
 
-    # 按月计算每月老客人数
-    repurchase = """SELECT DATE_FORMAT( pay_time, '%%Y-%%m') AS the_date, count( DISTINCT buyer_nick ) FROM friso_gold 
-                        WHERE pay_time >= "{}" AND pay_time < "{}"  
-                        AND buyer_nick IN (select buyer_nick from friso_gold_stat 
-                                        where first_purchase_time < "{}" and first_purchase_time >= "{}" )
-                        GROUP BY the_date""".format(start_date, end_date, start_date, date_diff)
+        # 需要一个参数，将时间减去2年
+        date_diff = start_date - relativedelta(years=2)
 
-    # 累计老客人数
-    customer = """select DATE_FORMAT("{}", '%%Y-%%m') AS the_date, count(distinct buyer_nick) from friso_gold_stat 
-                        where first_purchase_time <"{}" 
-                        and first_purchase_time > "{}" """.format(start_date, start_date, date_diff)
+        # 按月计算每月 近2年老客复购人数
+        repurchase = """SELECT DATE_FORMAT( pay_time, '%%Y-%%m') AS the_date, count( DISTINCT buyer_nick ) FROM triumph 
+                            WHERE pay_time >= "{}" AND pay_time < "{}"  
+                            AND buyer_nick IN (select buyer_nick from triumph_stat 
+                                            where first_purchase_time < "{}" and first_purchase_time >= "{}" )
+                            GROUP BY the_date""".format(start_date, end_date, start_date, )
+
+        # 累计近2年老客人数
+        customer = """select DATE_FORMAT("{}", '%%Y-%%m') AS the_date, count(distinct buyer_nick) from triumph_stat 
+                            where first_purchase_time <"{}" 
+                            and first_purchase_time > "{}" """.format(start_date, start_date, date_diff)
+    else:
+        # 按月计算每月老客人数
+        repurchase = """SELECT DATE_FORMAT( pay_time, '%%Y-%%m') AS the_date, count( DISTINCT buyer_nick ) FROM triumph 
+                            WHERE pay_time >= "{}" AND pay_time < "{}"  
+                            AND buyer_nick IN (select buyer_nick from triumph_stat 
+                                            where first_purchase_time < "{}" )
+                            GROUP BY the_date""".format(start_date, end_date, start_date)
+
+        # 累计老客人数
+        customer = """select DATE_FORMAT("{}", '%%Y-%%m') AS the_date, count(distinct buyer_nick) from triumph_stat 
+                            where first_purchase_time <"{}" """.format(start_date, start_date)
+
     return repurchase, customer
 
 
 if __name__ == '__main__':
 
-    # 输出文件名称
-    excel_name = r'美素佳儿复购和老客数据.xlsx'
+    # 需要查询的品牌名称,用英文
+    brand_name = 'triumph'
 
+    # 是否需要限定在2年内的老客,用于sql的选择
+    # 0为不限定，1为限定
+    if_limited = 0
+
+    # 开始日期
     s_date = datetime.date(2015, 1, 1)
     e_date = datetime.date(2015, 2, 1)
 
     # last_date用于计算月份差， 以确定for循环次数
     last_date = datetime.date(2019, 10, 1)
+
+    """
+    以上为需要修改的参数
+    --------------------------------------------------------------------------------------------
+    以下为运行参数和具体执行代码
+    """
+
+    # 输出文件名称
+    excel_name = brand_name + r'复购和老客数据.xlsx'
+
+    # 计算循环次数
     month_diff = abs((last_date.year - s_date.year) * 12 + (last_date.month - s_date.month) * 1)
     print(s_date, last_date, month_diff)
 
@@ -49,9 +79,9 @@ if __name__ == '__main__':
     repurchase_trend = DataFrame()
     customer_trend = DataFrame()
 
-    for i in range(0, month_diff):
+    for i in range(month_diff):
         # 获取按月循环执行的sql
-        repurchases, customers = store_sql(s_date, e_date)
+        repurchases, customers = store_sql(s_date, e_date, if_limit=if_limited)
         print(repurchases, customers)
 
         df_repurchases = pd.read_sql(repurchases, con=db_brand)
